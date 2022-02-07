548f1219e44b729610429b12c909b2acc46166c8
Fabio Leardini <leardinifabio90@gmail.com>
2022-02-07 01:37:19 -0300

    iniciando algoritimo

--- js/campo-minado.js
+++ js/campo-minado.js
@@ -11,27 +11,27 @@ function CampoMinado() {
     var JogoTotalDeCasasAbertas = 34;
 
     var Solucao = [
-        [ "0", "0", "0", "0", "0", "0", "0", "1", "*" ],
-        [ "0", "0", "0", "0", "0", "0", "0", "1", "1" ],
-        [ "1", "2", "2", "1", "0", "0", "0", "0", "0" ],
-        [ "1", "*", "*", "3", "2", "2", "1", "1", "0" ],
-        [ "1", "2", "3", "*", "*", "4", "*", "2", "1" ],
-        [ "0", "0", "1", "3", "*", "*", "3", "*", "2" ],
-        [ "0", "0", "0", "1", "2", "2", "2", "2", "*" ], 
-        [ "0", "0", "0", "0", "0", "0", "0", "1", "1" ],
-        [ "0", "0", "0", "0", "0", "0", "0", "0", "0" ]
+        ["0", "0", "0", "0", "0", "0", "0", "1", "*"],
+        ["0", "0", "0", "0", "0", "0", "0", "1", "1"],
+        ["1", "2", "2", "1", "0", "0", "0", "0", "0"],
+        ["1", "*", "*", "3", "2", "2", "1", "1", "0"],
+        ["1", "2", "3", "*", "*", "4", "*", "2", "1"],
+        ["0", "0", "1", "3", "*", "*", "3", "*", "2"],
+        ["0", "0", "0", "1", "2", "2", "2", "2", "*"],
+        ["0", "0", "0", "0", "0", "0", "0", "1", "1"],
+        ["0", "0", "0", "0", "0", "0", "0", "0", "0"]
     ];
 
     var Jogo = [
-        [ "0", "0", "0", "0", "0", "0", "0", "1", "-" ],
-        [ "0", "0", "0", "0", "0", "0", "0", "1", "1" ],
-        [ "1", "2", "2", "1", "0", "0", "0", "0", "0" ],
-        [ "-", "-", "-", "3", "2", "2", "1", "1", "0" ],
-        [ "-", "-", "-", "-", "-", "-", "-", "2", "1" ],
-        [ "-", "-", "-", "-", "-", "-", "-", "-", "-" ],
-        [ "-", "-", "-", "-", "-", "-", "-", "-", "-" ],
-        [ "-", "-", "-", "-", "-", "-", "-", "-", "-" ],
-        [ "-", "-", "-", "-", "-", "-", "-", "-", "-" ]
+        ["0", "0", "0", "0", "0", "0", "0", "1", "-"],
+        ["0", "0", "0", "0", "0", "0", "0", "1", "1"],
+        ["1", "2", "2", "1", "0", "0", "0", "0", "0"],
+        ["-", "-", "-", "3", "2", "2", "1", "1", "0"],
+        ["-", "-", "-", "-", "-", "-", "-", "2", "1"],
+        ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+        ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+        ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+        ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
     ];
 
     /**
@@ -49,16 +49,13 @@ function CampoMinado() {
     this.Tabuleiro = function () {
         var tabuleiro = "";
 
-        for (var x = 0; x < 9; x++)
-        {
-            for (var y = 0; y < 9; y++)
-            {
+        for (var x = 0; x < 9; x++) {
+            for (var y = 0; y < 9; y++) {
                 tabuleiro += Jogo[x][y];
             }
 
-            if (x != 8)
-            {
-                tabuleiro +=  "" + "\r\n";
+            if (x != 8) {
+                tabuleiro += "" + "\r\n";
             }
         }
 
@@ -77,93 +74,74 @@ function CampoMinado() {
      * @return void
      */
     this.Abrir = function (linha, coluna) {
-        if (linha >= 1 && linha <= 9 && coluna >= 1 && coluna <= 9)
-        {
+        if (linha >= 1 && linha <= 9 && coluna >= 1 && coluna <= 9) {
             linha--;
             coluna--;
 
-            if (Status == 2 || Solucao[linha][coluna] == "*")
-            {
+            if (Status == 2 || Solucao[linha][coluna] == "*") {
                 Jogo[linha][coluna] = Solucao[linha][coluna];
                 Status = 2;
-            }
-            else
-            {
-                if (Jogo[linha][coluna] == "-" && Solucao[linha][coluna] != "0")
-                {
+            } else {
+                if (Jogo[linha][coluna] == "-" && Solucao[linha][coluna] != "0") {
                     Jogo[linha][coluna] = Solucao[linha][coluna];
                     JogoTotalDeCasasAbertas++;
-                }
-                else if (Jogo[linha][coluna] == "-" && Solucao[linha][coluna] == "0")
-                {
-                    if (Jogo[4][0] == "-")
-                    {
+                } else if (Jogo[linha][coluna] == "-" && Solucao[linha][coluna] == "0") {
+                    if (Jogo[4][0] == "-") {
                         Jogo[4][0] = Solucao[4][0];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[4][1] == "-")
-                    {
+                    if (Jogo[4][1] == "-") {
                         Jogo[4][1] = Solucao[4][1];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[4][2] == "-")
-                    {
+                    if (Jogo[4][2] == "-") {
                         Jogo[4][2] = Solucao[4][2];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[5][2] == "-")
-                    {
+                    if (Jogo[5][2] == "-") {
                         Jogo[5][2] = Solucao[5][2];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[5][3] == "-")
-                    {
+                    if (Jogo[5][3] == "-") {
                         Jogo[5][3] = Solucao[5][3];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[6][3] == "-")
-                    {
+                    if (Jogo[6][3] == "-") {
                         Jogo[6][3] = Solucao[6][3];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[6][4] == "-")
-                    {
+                    if (Jogo[6][4] == "-") {
                         Jogo[6][4] = Solucao[6][4];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[6][5] == "-")
-                    {
+                    if (Jogo[6][5] == "-") {
                         Jogo[6][5] = Solucao[6][5];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[6][6] == "-")
-                    {
+                    if (Jogo[6][6] == "-") {
                         Jogo[6][6] = Solucao[6][6];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[6][7] == "-")
-                    {
+                    if (Jogo[6][7] == "-") {
                         Jogo[6][7] = Solucao[6][7];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[7][7] == "-")
-                    {
+                    if (Jogo[7][7] == "-") {
                         Jogo[7][7] = Solucao[7][7];
                         JogoTotalDeCasasAbertas++;
                     }
 
-                    if (Jogo[7][8] == "-")
-                    {
+                    if (Jogo[7][8] == "-") {
                         Jogo[7][8] = Solucao[7][8];
                         JogoTotalDeCasasAbertas++;
                     }
@@ -192,8 +170,7 @@ function CampoMinado() {
                     JogoTotalDeCasasAbertas += 21;
                 }
 
-                if (JogoTotalDeCasasAbertas == 71)
-                {
+                if (JogoTotalDeCasasAbertas == 71) {
                     Status = 1;
                 }
             }
